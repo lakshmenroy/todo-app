d34d982157c828da4127ce87eade3691966937a8
lakshmenroy <laksh.menroy.21@gmail.com>
2024-06-10 22:52:29 +0530

    Initial commit

--- app.py
+++ app.py
@@ -8,15 +8,16 @@ def add_todo():
     todo = st.session_state["new_todo"] + '\n'
     todos.append(todo)
     functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
 
 
 st.title('Todo List')
 for idx, td in enumerate(todos):
-    checkbox = st.checkbox(td, key=td)
+    checkbox = st.checkbox(td, key=f"{td}-{idx}")
     if checkbox:
         todos.pop(idx)
         functions.write_todos(todos)
-        del st.session_state[td]
+        del st.session_state[f"{td}-{idx}"]
         st.rerun()
 
 st.text_input(label=' ', placeholder='Add a new item...',
